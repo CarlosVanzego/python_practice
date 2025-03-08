0eb8f2e25955442f174accab6cc363578489a4ea
Your Name <carlosvanzego96@gmail.com>
2025-03-08 00:21:05 -0600

    is_even commit

---
+++ is_even.py
@@ -0,0 +1,7 @@
+# the function below checks if a value is even or odd using an if statement and the modulo operator:
+# 
+def is_even(number):
+    if number % 2 == 0:
+      return True
+    else:
+       return False
