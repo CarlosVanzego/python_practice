41a50804a74ba2e7eaf5adfd61264920f8111a40
Your Name <carlosvanzego96@gmail.com>
2025-03-08 13:18:47 -0600

    for_loops.py update

--- for_loops.py
+++ for_loops.py
@@ -8,3 +8,64 @@ i = 0
 while i < len(numbers):
     print(numbers[i])
     i = i + 1  
+
+
+
+
+
+# Question:
+# You want to print 3 numbers from a list.
+
+
+# Solution:
+for i in [8, 9, 10]:
+    print(i)
+
+
+# Question:
+# You want to print the message “Access denied” 5 times.
+for i in range(5):
+    print("Access denied")
+
+
+
+# Question:
+# You want to print out a sequence of numbers starting at 10 and ending at 30.
+for i in range(10, 31):
+    print(i)
+
+
+
+# Question:
+# You want to print a message that tells the user to “try again” as long as the value of the attempt variable is 5 or less, and you want to increase the value of this variable by 1 each time it passes through the loop.
+attempt = 1
+while attempt <= 5:
+    print("Try Again.")
+attempt = attempt + 1
+
+
+# Question:
+# You want to print out the numbers 20, 19, 18, 17, and 16.
+i = 20
+while (i > 15):
+    print(i)
+
+    i = i - 1
+
+
+
+
+# Question:
+# You want to welcome 3 users from a list by their name (for example, “Welcome, Emerick Larson”).
+name = ["Emrick Larson", "Estrella Ortiz", "Tori Shah"]
+
+for i in name:
+    print("Welcome, ", i)
+
+
+
+
+
+
+
+
--- while_loop.py
+++ while_loop.py
@@ -9,24 +9,24 @@
 #   i = i + 1
 
 
-# Example while loop:
-x = 0
-while x < 5:
-  print("Not there yet, x=" + str(x))
-  x = x + 1
-print("x=" + str(x))
+# # Example while loop:
+# x = 0
+# while x < 5:
+#   print("Not there yet, x=" + str(x))
+#   x = x + 1
+# print("x=" + str(x))
 
 
 
-# Another example while loop:
-def attempts(n):
-    x = 1
-    while x <= n:
-        print("Attempt " + str(x))
-        x += 1
-    print("Done")
+# # Another example while loop:
+# def attempts(n):
+#     x = 1
+#     while x <= n:
+#         print("Attempt " + str(x))
+#         x += 1
+#     print("Done")
     
-attempts(5)
+# attempts(5)
 
 # Question
 # In this code, there's an initialization problem that's causing our function to behave incorrectly. Can you find the problem and fix it?
@@ -41,14 +41,14 @@ attempts(5)
 
 
 # Solution:
-def count_down(start_number):
-  current = start_number
-  while (current > 0):
-    print(current)
-    current -= 1
-  print("Zero!")
+# def count_down(start_number):
+#   current = start_number
+#   while (current > 0):
+#     print(current)
+#     current -= 1
+#   print("Zero!")
 
-count_down(3)
+# count_down(3)
 
 
 
@@ -68,13 +68,13 @@ count_down(3)
 # Question:
 # Reflect
 # The following code causes an infinite loop. Can you figure out what’s missing and how to fix it?
-def print_range(start, end):
-	# Loop through the numbers from start to end
-	x = start
-	while x <= end:
-		print(x)
+# def print_range(start, end):
+# 	# Loop through the numbers from start to end
+# 	x = start
+# 	while x <= end:
+# 		print(x)
 
-print_range(1, 5)  # Should print 1 2 3 4 5 (each number on its own line)
+# print_range(1, 5)  # Should print 1 2 3 4 5 (each number on its own line)
 
 
 
@@ -97,13 +97,13 @@ print_range(1, 5)  # Should print 1 2 3 4 5 (each number on its own line)
 
 
 # Ex while loop
-multiplier = 2
-result = multiplier * 4
-while result <= 100:
-    print(result)
-    multiplier += 1
-    result = multiplier * 4
-print("Finished")
+# multiplier = 2
+# result = multiplier * 4
+# while result <= 100:
+#     print(result)
+#     multiplier += 1
+#     result = multiplier * 4
+# print("Finished")
 
 
 # This while loop prints the multiples of 4 between 2 and 100. The
@@ -130,3 +130,191 @@ print("Finished")
 
 
 
+# Question:
+# The following code contains an infinite loop, meaning the Python interpreter does not know when to exit the loop once the task is complete. To solve the problem, you will need to:
+
+# Find the error in the code
+
+# Fix the while loop so there is an exit condition
+
+# Hint: Try running your function with the number 0 as the input and observe the result.
+
+# Note that Coursera’s code blocks will time out after 5 seconds of running an infinite loop. If you get this timeout error message, it means the infinite loop has not been fixed. 
+# 
+def is_power_of_two(number):
+  # This while loop checks if the "number" can be divided by two
+  # without leaving a remainder. How can you change the while loop to
+  # avoid a Python ZeroDivisionError?
+  while number % 2 == 0:
+    number = number / 2
+  # If after dividing by 2 "number" equals 1, then "number" is a power
+  # of 2.
+  if number == 1:
+    return True
+  return False
+  
+# Calls to the function
+print(is_power_of_two(0)) # Should be False
+print(is_power_of_two(1)) # Should be True
+print(is_power_of_two(8)) # Should be True
+print(is_power_of_two(9)) # Should be False
+
+
+
+
+
+
+# Solution
+def is_power_of_two(number):
+  # This while loop checks if the "number" can be divided by two
+  # without leaving a remainder. How can you change the while loop to
+  # avoid a Python ZeroDivisionError?
+  while number > 1 and number % 2 == 0:
+    number = number / 2
+  # If after dividing by 2 "number" equals 1, then "number" is a power
+  # of 2.
+  if number == 1:
+    return True
+  return False
+  
+# Calls to the function
+print(is_power_of_two(0)) # Should be False
+print(is_power_of_two(1)) # Should be True
+print(is_power_of_two(8)) # Should be True
+print(is_power_of_two(9)) # Should be False
+
+
+
+
+# Question:
+# Write a function that takes an argument n and returns the sum of integers from 1 to n. For example, if n=5, your function should add up 1+2+3+4+5 and return 15. If n is less than 1, just return 0. Use a while loop to calculate this sum. 
+# 
+def sum_of_integers(n):
+  if n < 1:
+    return 0
+
+
+  # i = 1
+  # sum = ___
+  # while ___:
+  #   sum = sum + i
+  #   i = ___
+
+
+  return sum
+
+
+print(sum_of_integers(3))  # should print 6
+print(sum_of_integers(4))  # should print 10
+print(sum_of_integers(5))  # should print 15
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Solution: 
+def sum_of_integers(n):
+  if n < 1:
+    return 0
+
+
+  i = 1
+  sum = 0
+  while i <= n:
+    sum = sum + i
+    i = i + 1
+
+
+  return sum
+
+
+print(sum_of_integers(3))  # should print 6
+print(sum_of_integers(4))  # should print 10
+print(sum_of_integers(5))  # should print 15
+
+
+
+
+# 5.
+# Question 5
+# Fill in the blanks to complete the function, which should output a multiplication table. The function accepts a variable “number” through its parameters. This “number” variable should be multiplied by the numbers 1 through 5, and printed in a format similar to “1x6=6” (“number” x “multiplier” = “result”). The code should also limit the “result” to not exceed 25. To satisfy these conditions, you will need to:
+
+# Initialize the “multiplier" variable with the starting value
+
+# Complete the while loop condition
+
+# Add an exit point for the loop
+
+# Increment the “multiplier" variable inside the while loop
+# 
+def multiplication_table(number):
+    # Initialize the appropriate variable
+    multiplier = 0
+
+
+    # Complete the while loop condition.
+    while number <= 25:
+        result = number * multiplier 
+        if  result > 25:
+            # Enter the action to take if the result > 25
+            break
+        print(str(number) + "x" + str(multiplier) + "=" + str(result))
+        
+        # Increment the appropriate variable
+        number += 1
+
+
+
+multiplication_table(3) 
+# Should print: 
+# 3x1=3 
+# 3x2=6 
+# 3x3=9 
+# 3x4=12 
+# 3x5=15
+
+
+multiplication_table(5) 
+# Should print: 
+# 5x1=5
+# 5x2=10
+# 5x3=15
+# 5x4=20
+# 5x5=25
+
+
+multiplication_table(8) 
+# Should print:
+# 8x1=8
+# 8x2=16
+# 8x3=24        
+
+
+
+# Solution
+def multiplication_table(number):
+    # Initialize the appropriate variable
+    multiplier = 1
+
+
+    # Complete the while loop condition.
+    while number <= 25:
+        result = number * multiplier 
+        if  result > 25:
+            # Enter the action to take if the result > 25
+            break
+        print(str(number) + "x" + str(multiplier) + "=" + str(result))
+        
+        # Increment the appropriate variable
+        multiplier = multiplier + 1
