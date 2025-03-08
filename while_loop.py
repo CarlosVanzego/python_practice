1cd141f3db3b7766a514385ad396786669113c81
Your Name <carlosvanzego96@gmail.com>
2025-03-08 08:26:41 -0600

    while_loop.py commit

--- while_loop.py
+++ while_loop.py
@@ -53,14 +53,14 @@ count_down(3)
 
 
 	# this is an infinite loop:
-def countNumbers(start_number):
-    current = start_number
-    while(current > 0):
-       print(current)
-       current += 1
-       print("infinite")
+# def countNumbers(start_number):
+#     current = start_number
+#     while(current > 0):
+#        print(current)
+#        current += 1
+#        print("infinite")
 
-countNumbers(1)     
+# countNumbers(1)     
 
 
 
@@ -85,14 +85,46 @@ print_range(1, 5)  # Should print 1 2 3 4 5 (each number on its own line)
 
 
 # Solution:
-def print_range(start, end):
-	# Loop through the numbers infinitely
-	x = start
-	while x <= end:
-		print(x)
-		x += 1 # or x = x + 1 
+# def print_range(start, end):
+# 	# Loop through the numbers infinitely
+# 	x = start
+# 	while x <= end:
+# 		print(x)
+# 		x += 1 # or x = x + 1 
 
-print_range(1, 5)  # Should print 1 2 3 4 5 (each number on its own line)
+# print_range(1, 5)  # Should print 1 2 3 4 5 (each number on its own line)
+
+
+
+# Ex while loop
+multiplier = 2
+result = multiplier * 4
+while result <= 100:
+    print(result)
+    multiplier += 1
+    result = multiplier * 4
+print("Finished")
+
+
+# This while loop prints the multiples of 4 between 2 and 100. The
+# "multiplier" variable is initialized with the starting value of 2. 
+# The "result" variable is initialized with the value of the 
+# "multiplier" variable times 4. 
+
+# The while loop specifies that the loop must iterate while it is True
+# that the "result" is less than or equal to 100. Within the while loop, 
+# the code tells the Python interpreter to print the value of the 
+# "result" variable. Then, the "multiplier" is incremented by 1 and the
+# "result" is assigned the new value of the "multiplier" times 4. 
+
+# The end of the while loop is indicated by the indentation of the next 
+# line of code moving one tab to the left. At this point, the Python
+# interpreter automatically loops back to the beginning of the while
+# loop to check the condition again with the new value of the "result"
+# variable. When the while loop condition becomes False (meaning
+# "result" is no longer less than or equal to 100), the interpreter exits
+# the loop and reads the next line of code outside of the loop. In this 
+# case, that next line tells the interpreter to print the string "Finished". 
 
 
 
