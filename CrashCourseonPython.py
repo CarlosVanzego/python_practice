444eed38ed29d65ce7e9c1448a3c820deaa0df3b
Your Name <carlosvanzego96@gmail.com>
2025-03-07 21:43:08 -0600

    Him commit

--- CrashCourseonPython.py
+++ CrashCourseonPython.py
@@ -53,3 +53,20 @@ print("Each person has to pay: " + str(share_per_person)) # change a data type
 
 
 
+# The following 5 lines assign strings to a list of variables.
+salutation = "Mr."
+first_name = "Carlos"
+middle_name = "Anthony"
+last_name = "Vanzego"
+suffix = "Him."
+ 
+print(salutation + " " + first_name + " " + middle_name + " " + last_name + ", " + suffix) 
+# The comma as a string ", " adds the conventional use of a comma plus a 
+# space to separate the last name from the suffix.
+ 
+# Alternatively, you could use commas in place of the + connector:
+print(salutation, first_name, middle_name, last_name,",", suffix)
+# However, you will find that this produces a space before a comma within a string.
+
+
+
