0d01cbd59d64f20c0452d3d5813286520934f719
Your Name <carlosvanzego96@gmail.com>
2025-03-08 07:35:08 -0600

    while_loop.py commit

--- while_loop.py
+++ while_loop.py
@@ -8,8 +8,44 @@
 #   print(i * '*')
 #   i = i + 1
 
+
+# Example while loop:
 x = 0
 while x < 5:
   print("Not there yet, x=" + str(x))
   x = x + 1
 print("x=" + str(x))
+
+
+
+# Another example while loop:
+def attempts(n):
+    x = 1
+    while x <= n:
+        print("Attempt " + str(x))
+        x += 1
+    print("Done")
+    
+attempts(5)
+
+# Question
+# In this code, there's an initialization problem that's causing our function to behave incorrectly. Can you find the problem and fix it?
+# 
+def count_down(start_number):
+  while (current > 0):
+    print(current)
+    current -= 1
+  print("Zero!")
+
+count_down(3)
+
+
+# Solution:
+def count_down(start_number):
+  current = start_number
+  while (current > 0):
+    print(current)
+    current -= 1
+  print("Zero!")
+
+count_down(3)
