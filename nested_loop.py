cb4c1b21f873025d36c056c8d38fdb777c213437
Your Name <carlosvanzego96@gmail.com>
2025-03-08 13:51:54 -0600

    nested_loop.py update

--- nested_loop.py
+++ nested_loop.py
@@ -1,18 +1,40 @@
-for left in range(7):
-  for right in range(left, 7):
-    print("[" + str(left) + "|" + str(right) + "]", end=" ")
-  print()
+# for left in range(7):
+#   for right in range(left, 7):
+#     print("[" + str(left) + "|" + str(right) + "]", end=" ")
+#   print()
 
 
 
 
-# Another Ex.
-teams = ['Dragons','Wolves', 'Pandas', 'Unicorns']
+# # Another Ex.
+# teams = ['Dragons','Wolves', 'Pandas', 'Unicorns']
 
-for home_team in teams:
-  for away_team in teams:
-    if home_team != away_team:
-      print(home_team + " vs " + away_team)
+# for home_team in teams:
+#   for away_team in teams:
+#     if home_team != away_team:
+#       print(home_team + " vs " + away_team)
+
+
+
+# Practice Question: Which option would fix this for loop to print the numbers 12, 18, 24, 30, 36?
+for n in range(6,18+1,3):
+  print(n*2)   
+
+
+
+
+
+# Practice Question: Which for loops will print all even numbers from 0 to 18? Select all that apply.
+for n in range(19):
+    if n % 2 == 0:
+        print(n)
+
+# and
+
+for n in range(10):
+  print(n+n)
+      
+
 
 
 
