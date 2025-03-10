c174867d99967799d64ef2e61db0309181402677
Your Name <carlosvanzego96@gmail.com>
2025-03-10 15:17:37 -0400

    writing files commit

---
+++ Writing_Files/write_file_path.py
@@ -0,0 +1,15 @@
+# Windows file directory
+# C: \my-directory\target-file.txt
+
+# Windows file directory written in Python
+# C: /my-directory/target-file.txt
+
+#Windows file directory
+# C:\\my-directory\\target-file.txt
+
+# this command is to use the current directory.
+#CWD command: 
+# os.getcwd()
+
+#CWD command for external files:
+# outputs['current_directory_before'] = os.getcwd()
