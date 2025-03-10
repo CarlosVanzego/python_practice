cc79f117a500ee3a03c3e1e7dc80ac505eee1540
Your Name <carlosvanzego96@gmail.com>
2025-03-09 23:22:56 -0400

    automation_practice.py update and comments

--- automation_practice.py
+++ automation_practice.py
@@ -1,3 +1,10 @@
+# This is example is for a scenario where you may want to check the health of your computer.
+# As this may require various tests, we can create a script to check each of these values all at once.
+# We can use the shutil module, and the disk_usage function to check for the available disk space
+# We can also the psutil and the cpu_percent function that will return a number showing how much of the CPU is being used.
+# The script below conducts both of these checks together:
+# 
+
 # !/user/bin/env python3
 import shutil
 import psutil
